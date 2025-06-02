272c91ad86577cc7d99f5a5cc19b1879788d63eb
Siddhu Mohan <61168189+Ranger-26@users.noreply.github.com>
2025-06-02 17:37:22 -0500

    finish rpilever

--- RPILever.py
+++ RPILever.py
@@ -8,41 +8,30 @@ except:
 
 
 class RPILever(LeverBase):
-    def __init__(self, lever_name: str, input_pin:int, output_pin:int):
+    def __init__(self, lever_name: str, input_pin:int):
         super().__init__(lever_name)
         self.input_pin = input_pin #pin that reads in if the lever is being pressed
-        self.output_pin = output_pin #pin that controls the lever active property
-        self.stateCount = 0
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.input_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self.output_pin, GPIO.OUT)
+        GPIO.setup(self.input_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def update_state_continously(self) -> None:
         input_pin_val = -1
         for i in range(30):
             input_pin_val = GPIO.input(self.input_pin)
         if self.state != input_pin_val:
-            print(str(self.stateCount)+") New State: "+str(input_pin_val))
-            self.stateCount += 1
+            print(self.name+" State: "+str(input_pin_val))
         self.set_state(input_pin_val)
         #check for state via output pin of lever
     def set_is_active(self, val: bool):
         super().set_is_active(val)
-        #Set relay voltage to this
-        if self.active:
-            GPIO.output(self.output_pin, 1)
-        else:
-            GPIO.output(self.output_pin, 0)
+        
 
 if __name__ == "__main__":
-    print("Testing Hardware Lever...")
-    import time 
-
-    lever:LeverBase = RPILever("rpi_lever", 25, 24)
-    print("Test 0")
-    lever.set_is_active(False)
-    time.sleep(2)
-    lever.set_is_active(not lever.active)
-    print("Test 1")
+    lever_1:LeverBase = RPILever("rpi_1_lever", 14)
+    lever_2:LeverBase = RPILever("rpi_2_lever", 15)
     while True:
-        lever.update_state_continously()
+        lever_1.update_state_continously()
+        lever_2.update_state_continously()
+        
+    
+        
