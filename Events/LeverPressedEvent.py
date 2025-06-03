efbe6fb6bf723d6770229d26f29db01b9b8e0518
Siddhu Mohan <61168189+Ranger-26@users.noreply.github.com>
2025-06-03 14:51:06 -0500

    add file

---
+++ Events/LeverPressedEvent.py
@@ -0,0 +1,20 @@
+from LeverEventBase import LeverEventBase
+from LeverBase import LeverBase, STATE_PRESSED
+from typing import Callable, Optional, Dict
+
+
+
+
+class LeverPressedEvent(LeverEventBase):
+    def __init__(
+        self,
+        event_name: str,
+        lever: LeverBase,
+        callback_fn: Optional[Callable[[LeverBase], None]]
+    ) -> None:
+        super().__init__(event_name, lever)
+        self.callback_fn: Optional[Callable[[LeverBase], None]] = callback_fn
+
+    def on_lever_state_change(self, new_lever_state: int) -> None:
+        if new_lever_state == STATE_PRESSED and self.callback_fn:
+            self.callback_fn(self.lever)
