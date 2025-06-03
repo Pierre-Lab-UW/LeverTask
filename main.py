152230c0bfeef8892fa77fbe1dd4cdb9e139dab9
Siddhu Mohan <61168189+Ranger-26@users.noreply.github.com>
2025-06-03 14:52:59 -0500

    add update for training

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from typing import Dict
 from LeverEventBase import DebugEvent
 from RPILever import RPILever
+from Training import Training
 from Trainings.FixedRatioTraining.FixedRatioTraining import FixedRatioTraining
 
 
@@ -10,10 +12,11 @@ lever_2:RPILever = RPILever("rpi_2_lever", 15)
 lever_1.add_event(DebugEvent("debug", lever_1))
 lever_2.add_event(DebugEvent("debug", lever_2))
 #start a fixed ratio training
-fixed_ratio_parameters = {"lever_presses":5, "update_interval":3}
-fixed_ratio_training = FixedRatioTraining(lever_1 , lever_2, fixed_ratio_parameters)
+fixed_ratio_parameters: Dict[str, int] = {"lever_presses":5, "update_interval":3}
+fixed_ratio_training: Training = FixedRatioTraining(lever_1 , lever_2, fixed_ratio_parameters)
 fixed_ratio_training.start_event()
 
 while True:
     lever_1.update()
     lever_2.update()
+    fixed_ratio_training.update()
