1d12f7543c546d9e16f8f146c230d3aed12cb57a
Siddhu Mohan <61168189+Ranger-26@users.noreply.github.com>
2025-06-10 17:34:40 -0500

    fix formatting issues for python3.7?

--- Trainings/FixedRatioTraining.py
+++ Trainings/FixedRatioTraining.py
@@ -31,7 +31,9 @@ class FixedRatioTraining(Training):
         self.last_reset_time: int = 0
         self.current_ratio = self.get_param("FR")
 
-        self.output_data_file = f"OutputData/FR{self.current_ratio}_data_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+        self.output_data_file = "OutputData/FR{}_data_{}.csv".format(
+            self.current_ratio, datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            )
         self.start_time = 0
 
         self.ITI = self.get_param("ITI")
@@ -155,7 +157,6 @@ class FixedRatioTraining(Training):
             #timeout logic - program will only timeout if any lever hasn't been pressed in the past [Timeout] seconds
             #will not timeout if monkey is holding lever for an extended amount of time, also the ITI cooldown is not counted in the timeout
             flag = True
-            print(now - self.last_lever_press_time)
             if now - self.last_lever_press_time <= self.timeout_time:
                 flag = False
             if self.lever1.get_state() != 1 and self.lever2.get_state() != 1:
--- Trainings/ProgressiveRatioTraining.py
+++ Trainings/ProgressiveRatioTraining.py
@@ -30,7 +30,6 @@ class ProgressiveRatioTraining(FixedRatioTraining):
             #timeout logic - program will only timeout if any lever hasn't been pressed in the past [Timeout] seconds
             #will not timeout if monkey is holding lever for an extended amount of time, also the ITI cooldown is not counted in the timeout
             flag = True
-            print(now - self.last_lever_press_time)
             if now - self.last_lever_press_time <= self.timeout_time:
                 flag = False
             if self.lever1.get_state() != 1 and self.lever2.get_state() != 1:
