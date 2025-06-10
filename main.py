39695bae10cde99f71b1405a28997b921a7f3062
Siddhu Mohan <61168189+Ranger-26@users.noreply.github.com>
2025-06-10 17:29:56 -0500

    update main.py with new stuff

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from LeverEventBase import DebugEvent
 from RPILever import RPILever
 from Training import Training
 from Trainings.FixedRatioTraining import FixedRatioTraining
+from Trainings.ProgressiveRatioTraining import ProgressiveRatioTraining
 
 
 lever_1:RPILever = RPILever("rpi_1_lever", 14)
@@ -12,11 +13,13 @@ lever_2:RPILever = RPILever("rpi_2_lever", 15)
 lever_1.add_event(DebugEvent("debug", lever_1))
 lever_2.add_event(DebugEvent("debug", lever_2))
 #start a fixed ratio training
-fixed_ratio_parameters: Dict[str, int] = {"FR":5, "ITI":3, "PRP":5}
-fixed_ratio_training: Training = FixedRatioTraining(lever_1 , lever_2, fixed_ratio_parameters)
-fixed_ratio_training.start_event()
+fixed_ratio_parameters: Dict[str, int] = {"FR":5, "ITI":3, "PR":1, "Timeout":10}
+current_training: Training = ProgressiveRatioTraining(lever_1 , lever_2, fixed_ratio_parameters)
+current_training.start_event()
 
 while True:
     lever_1.update()
     lever_2.update()
-    fixed_ratio_training.update()
+    current_training.update()
+    if current_training.should_end_traning():
+        break
