cc919a7811c0761c2f90f2f463a22b33a4ac1c8c
Siddhu Mohan <61168189+Ranger-26@users.noreply.github.com>
2026-02-10 12:48:13 -0600

    yes

--- Bluetooth/server.py
+++ Bluetooth/server.py
@@ -46,7 +46,7 @@ class TrainingBluetoothServer(BluetoothServerBase):
             print("File not found:", filename)
             print("Available files:\n", avalible_files)
 
-            avalible_files_message: str = "ERR: File not found!\nAvailable Files:\n" + "\n".join(os.listdir(self.output_dir))
+            avalible_files_message: str = "ERR: File not found! Available Files:" + " ".join(os.listdir(self.output_dir))
             #send all files names in output dir to client for user convenience
             self.send_message(avalible_files_message)
             return
